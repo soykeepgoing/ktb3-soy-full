99627bec7ce1045519521bf9a80af27551b1414e
soykeepgoing <soykeepgoing@gmail.com>
2025-11-08 17:06:34 +0900

    re

--- springcommunity/src/main/java/com/soy/springcommunity/controller/UsersController.java
+++ springcommunity/src/main/java/com/soy/springcommunity/controller/UsersController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500/")
 @RequestMapping("/api/users")
 public class UsersController {
     private UsersService usersService;
